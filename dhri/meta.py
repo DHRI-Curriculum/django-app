5b1e0a3819bfe427f75db816ae5e9f6089363a1b
Kalle Westerling <kwesterling1@gradcenter.cuny.edu>
2020-06-16 12:20:59 -0400

    consistency

--- dhri/meta.py
+++ dhri/meta.py
@@ -4,14 +4,17 @@ from .constants import URL, AUTO_RESET
 from .log import dhri_log, dhri_warning, dhri_error, dhri_input
 
 
-def get_argparser():
+def get_argparser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-d", "--download", type=str, help="download from a directory")
-    group.add_argument("-f", "--file", type=str, help="load data from a file containing JSON with processed data from a DHRI curriculum")
-    group.add_argument("-r", "--reset", action='store_true', help="reset the DHRI curriculum data in the database")
-    group2 = group.add_argument_group()
-    group2.add_argument('--dest', type=str, help="optional destination for download")
+    
+    g = parser.add_mutually_exclusive_group(required=True)
+    g.add_argument('-d', '--download', type=str, help='download from a directory')
+    g.add_argument('-f', '--file', type=str, help='load data from a file containing JSON with processed data from a DHRI curriculum')
+    g.add_argument('-r', '--reset', action='store_true', help='reset the DHRI curriculum data in the database')
+    
+    g2 = group.add_argument_group()
+    g2.add_argument('--dest', type = str, help = 'optional destination for download')
+    
     return(parser)
 
 
@@ -24,18 +27,18 @@ def confirm_url(url: str) -> bool:
 
 def verify_url(url: str) -> str:
     if not confirm_url(url):
-        dhri_error("-d must provide a valid URL to a DHRI repository.")
-    if not "github" in url.lower():
-        dhri_error(f"Your URL seems to not originate with Github. Currently, our curriculum only works with Github as backend.") # Set to kill out of the program
-    dhri_log(f"URL accepted: {url}")
+        dhri_error('-d must provide a valid URL to a DHRI repository.')
+    if not 'github' in url.lower():
+        dhri_error(f'Your URL seems to not originate with Github. Currently, our curriculum only works with Github as backend.') # Set to kill out of the program
+    dhri_log(f'URL accepted: {url}')
     return(url)
 
 
 def load_data(path: str) -> dict:
-  dhri_log(f"Loading {path}")
+  dhri_log(f'Loading {path}')
 
   if not Path(path).exists():
-    dhri_error(f"Could not find JSON file on path: {path}")
+    dhri_error(f'Could not find JSON file on path: {path}')
 
   data = json.loads(Path(path).read_text())
   return(data)
@@ -43,21 +46,21 @@ def load_data(path: str) -> dict:
 
 def test_path(path: str) -> bool:
     """ Tests the pathname """
-    if not path.endswith(".json"):
-        dhri_warning(f"The data file ({path}) is not saved as a .json file. It will work but it might be confusing.")
+    if not path.endswith('.json'):
+        dhri_warning(f'The data file ({path}) is not saved as a .json file. It will work but it might be confusing.')
     return(True)
 
 
 def save_data(path: str, data: dict) -> bool:
     test_path(path)
     Path(path).write_text(json.dumps(data))
-    dhri_log(f"File saved to {path}")
+    dhri_log(f'File saved to {path}')
     return(True)
 
 
 def get_or_default(message: str, default_variable: str) -> str:
-    _ = input(f"{message} (default '{default_variable}'): ")
-    if _ != "":
+    _ = input(f'{message} (default "{default_variable}"): ')
+    if _ != '':
         return(_)
     else:
         return(default_variable)
@@ -65,13 +68,13 @@ def get_or_default(message: str, default_variable: str) -> str:
 
 def reset_all(kill=True) -> None:
     if AUTO_RESET == False:
-      _continue = dhri_input("Are you sure you want to reset the entire DHRI curriculum in the current Django database? (y/N) ", bold=True, color="red")
-      if _continue.lower() != "y":
+      _continue = dhri_input('Are you sure you want to reset the entire DHRI curriculum in the current Django database? (y/N) ', bold=True, color='red')
+      if _continue.lower() != 'y':
         exit()
     else:
-      dhri_warning("Resetting database (AUTO_RESET set to True)...")
+      dhri_warning('Resetting database (AUTO_RESET set to True)...')
 
     from dhri.backend import Workshop, Frontmatter, Project, Resource, Literature, Contributor
     for _ in [Workshop, Frontmatter, Project, Resource, Literature, Contributor]:
         _.objects.all().delete()
-    dhri_log(f"All {_.__name__} deleted.", kill=not AUTO_RESET)
+    dhri_log(f'All {_.__name__} deleted.', kill=not AUTO_RESET)
