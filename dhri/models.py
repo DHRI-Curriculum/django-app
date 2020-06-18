da97b55ed956d67203ee88dbd53621f532e3f33e
Kalle Westerling <kwesterling1@gradcenter.cuny.edu>
2020-06-18 16:34:48 -0400

    adding models

--- dhri/models.py
+++ dhri/models.py
@@ -1,3 +1,4 @@
 from workshop.models import Workshop
 from frontmatter.models import Frontmatter, Project, Resource, Reading, Contributor
 from assessment.models import Question, Answer, QuestionType
+from praxis.models import Tutorial, Praxis
