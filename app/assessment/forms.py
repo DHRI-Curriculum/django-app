16a68d18d249473ddacd404e17c545399f647cf0
Kalle Westerling <kwesterling1@gradcenter.cuny.edu>
2020-06-17 22:02:41 -0400

    rudimentary assessment set up

--- app/assessment/forms.py
+++ app/assessment/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class TestForm(forms.Form):
-    test_question = forms.CharField(label='Test question', max_length=100)
+    pass # test_question = forms.CharField(label='Test question', max_length=100)
--- app/assessment/views.py
+++ app/assessment/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.views import View
 from .models import Question
 from .forms import TestForm
 
 
-def test(request, question_id):
-    form = TestForm()
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'assessment/test.html', {'question': question, 'form': form})
+class Test(View):
+
+    form_class = TestForm
+    initial_values = {}
+
+    def get(self, request, question_id, *args, **kwargs):
+        form = self.form_class(initial=self.initial_values)
+
+        question = get_object_or_404(Question, pk=question_id)
+        
+        if question.question_type.label.lower() in ['multi-choice', 'radio', 'checkbox', 'select']:
+            question.form_setting = "values"
+        return render(request, 'assessment/test.html', {'question': question, 'form': form})
+
+        #return HttpResponse("Nothing to see here.")
+
+    def post(self, request, question_id, *args, **kwargs):
+        form = self.form_class(request.POST)
+        return HttpResponse(f"POST received for {question_id}: {request.POST}. args: {args}. kwargs: {kwargs}")
+
 
 
 def index(request):
