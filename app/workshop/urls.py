7d6ed127ff7b32f296dbfd5d468d5b6e491041b9
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-06-23 19:46:12 -0400

    Resolves Lift in some styling from dhinstitutes.org #67

--- app/workshop/urls.py
+++ app/workshop/urls.py
@@ -8,5 +8,7 @@ app_name = 'workshop'
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('<str:slug>/', views.detail, name='detail'),
+  path('<str:slug>/', views.frontmatter, name='frontmatter'),
+  path('<str:slug>/theory-to-practice/', views.praxis, name='praxis'),
+  path('<str:slug>/lessons/<str:lesson_slug>', views.lesson, name='lesson'),
 ]
--- app/workshop/views.py
+++ app/workshop/views.py
@@ -2,13 +2,10 @@ from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.core.exceptions import MultipleObjectsReturned
 from .models import Workshop
 
-def index(request):
-  workshops = Workshop.objects.all()
-  return render(request, 'workshop/index.html', {'workshops': workshops})
 
-def detail(request, slug=None):
+def _flexible_get(model=None, slug_or_int=''):
   try:
-    int(slug)
+    int(slug_or_int)
     is_id, is_slug = True, False
   except ValueError:
     is_id, is_slug = False, True
@@ -18,17 +15,36 @@ def detail(request, slug=None):
   if is_slug:
     try:
       multiple = False
-      obj = get_object_or_404(Workshop, slug=slug)
+      obj = get_object_or_404(model, slug=slug_or_int)
     except MultipleObjectsReturned:
       multiple = True
-      obj = Workshop.objects.filter(slug=slug).last()
-    response = f"You have selected workshop slug {obj.slug} ({obj.name})."
-    if multiple:
-      response += f"<br />This workshop exists multiple times in the database. The latest one is displayed."
+      obj = model.objects.filter(slug=slug_or_int).last()
+    return(multiple, obj)
   elif is_id:
-    obj = get_object_or_404(Workshop, pk=slug)
-    response = f"You have selected workshop slug {obj.slug} ({obj.name})."
+    obj = get_object_or_404(model, pk=slug_or_int)
+    return(False, obj)
   else:
-    response = f"You have not selected a workshop ID."
-  # return HttpResponse(response)
-  return render(request, 'workshop/detail.html', {'workshop': obj})
+    response = f"You have not selected a valid ID for the model {model}."
+    return HttpResponse(response, status=500)
+
+
+
+def index(request):
+  workshops = Workshop.objects.all()
+  return render(request, 'workshop/workshop-list.html', {'workshops': workshops})
+
+def frontmatter(request, slug=None):
+  _, obj = _flexible_get(Workshop, slug)
+  frontmatter = obj.frontmatter
+  return render(request, 'workshop/frontmatter.html', {'workshop': obj, 'frontmatter': frontmatter})
+
+def praxis(request, slug=None):
+  _, obj = _flexible_get(Workshop, slug)
+  frontmatter = obj.frontmatter
+  praxis = obj.praxis
+  return render(request, 'workshop/praxis.html', {'workshop': obj, 'frontmatter': frontmatter, 'praxis': praxis})
+
+def lesson(request, slug=None, lesson_slug=None):
+  _, obj = _flexible_get(Workshop, slug)
+  frontmatter = obj.frontmatter
+  return render(request, 'lesson/lesson.html', {'workshop': obj, 'frontmatter': frontmatter})
