bc480bc53812bc612e55b1aefb4dcc0b47fba643
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-06-23 00:58:12 -0400

    #67 first commit

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('workshop/', include('workshop.urls')),
     path('admin/', admin.site.urls),
     path('assessment/', include('assessment.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # handler404 = 'site.views.404'
