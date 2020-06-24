18d11a33604a583bfa8365b938e16798e4b66a3b
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-06-23 20:13:10 -0400

    and did something stupid

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website.apps.AssessmentConfig',
+    'website.apps.WebsiteConfig',
     'workshop.apps.WorkshopConfig',
     'frontmatter.apps.FrontmatterConfig',
     'assessment.apps.AssessmentConfig',
