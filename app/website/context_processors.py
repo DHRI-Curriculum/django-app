f1b98d3de767f8f253bb0ac4dc2ba8e61effd9a6
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-06-24 17:26:36 -0400

    fixing some website stylings

--- app/website/context_processors.py
+++ app/website/context_processors.py
@@ -1,8 +1,11 @@
-# from workshop.models import Workshop
+from workshop.models import Workshop
 
 from pathlib import Path
 
 def add_to_all_contexts(request):
     context_data = dict()
-    # context_data['all_workshops'] = Workshop.objects.all().count()
+
+    context_data['footer'] = dict()
+    context_data['footer']['workshops'] = Workshop.objects.all()
+
     return context_data
