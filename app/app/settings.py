9c3d6b3ad4b0e182f2b04b96f4b91294d951f095
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-06-24 12:09:39 -0400

    Capture Microsoft Explorer and generate warning language Fixes #71

--- app/app/settings.py
+++ app/app/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app.middleware.CatchInternetExplorer'
 ]
 
 ROOT_URLCONF = 'app.urls'
