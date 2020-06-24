ca995b32e8295b7d8d4e95c48b97e139481a803d
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-06-24 12:45:38 -0400

    more testing - this is frustrating

--- app/website/context_processors.py
+++ app/website/context_processors.py
@@ -4,6 +4,7 @@ def add_to_all_contexts(request):
     context_data = dict()
 
     user_agent = request.META['HTTP_USER_AGENT'].lower()
+    context_data['user_agent'] = user_agent
     context_data['is_ie'] = ('trident' in user_agent) or ('msie' in user_agent)
     # context_data['all_workshops'] = Workshop.objects.all().count()
     return context_data
