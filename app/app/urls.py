5a147bb639e138e8ae2dea66b5eccde699c9c093
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-06-24 17:12:01 -0400

    adding staticfiles_urlpatterns

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,8 @@ urlpatterns = [
     path('assessment/', include('assessment.urls')),
     path('', include('website.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # handler404 = 'site.views.404'
