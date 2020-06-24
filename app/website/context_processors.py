229181737ffd1870b172d262fdc2dbe6914b3608
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-06-24 12:53:19 -0400

    mote

--- app/website/context_processors.py
+++ app/website/context_processors.py
@@ -5,14 +5,17 @@ from pathlib import Path
 def add_to_all_contexts(request):
     context_data = dict()
 
+    user_agent = request.META['HTTP_USER_AGENT'].lower()
+    context_data['user_agent'] = user_agent
+    context_data['is_trident'] = 'trident' in user_agent
+    context_data['is_msie'] = 'msie' in user_agent
+
     string = ''
     path = Path('info.txt')
     if path.exists():
         string = path.read_text()
-    string = string + str(request.META) + '\n'
+    string = string + str(request.META['HTTP_USER_AGENT']) + '\n'
     path.write_text(string)
-    user_agent = request.META['HTTP_USER_AGENT'].lower()
-    context_data['user_agent'] = user_agent
-    context_data['is_ie'] = ('trident' in user_agent) or ('msie' in user_agent)
+
     # context_data['all_workshops'] = Workshop.objects.all().count()
     return context_data
