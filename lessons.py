6f46a0f5ffefd3c3b68cb3108ed3c4a5080e6c93
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-07-08 13:54:06 -0400

    testing files for lessons - can be deleted eventually

--- lessons.py
+++ lessons.py
@@ -1,11 +1,21 @@
 from dhri.utils.loader import Loader
-
-l = Loader('https://github.com/DHRI-Curriculum/git', 'v2.0-kristen-edits')
-
+from dhri.utils.parse_lesson import LessonParser
 import json
 from pathlib import Path
 
-lessons_txt = Path('lessons.txt')
-lessons_json = Path('lessons.json')
-lessons_txt.write_text(l._lessons_raw)
-lessons_json.write_text(json.dumps(l.lessons))
+
+l = Loader('https://github.com/DHRI-Curriculum/git', 'v2.0-kristen-edits')
+git_lessons = l._lessons_raw + '\n'
+git_lessons = LessonParser(git_lessons)
+
+l = Loader('https://github.com/DHRI-Curriculum/text-analysis', 'v2.0-rafa-edits')
+text_lessons = l._lessons_raw + '\n'
+text_lessons = LessonParser(text_lessons)
+
+l = Loader('https://github.com/DHRI-Curriculum/python', 'v2.0-filipa-edits')
+python_lessons = l._lessons_raw + '\n'
+python_lessons = LessonParser(python_lessons)
+
+l = Loader('https://github.com/DHRI-Curriculum/html-css', 'v2.0-param-edits')
+html_lessons = l._lessons_raw + '\n'
+html_lessons = LessonParser(html_lessons)
