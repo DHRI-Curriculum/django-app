c9ce9672b521f5ea393d4f7b1ad8c8f455893477
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-07-09 14:25:29 -0400

    adding possibility for pages

--- app/website/views.py
+++ app/website/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Page
 
 def index(request):
-  print('index!')
   page = Page.objects.filter(is_homepage=True).last()
   return render(request, 'website/index.html', {'page': page})
 
 def page(request, page_slug):
-  print('page!')
   page = get_object_or_404(Page, slug=page_slug)
-  return render(request, 'website/index.html', {'page': page})
+  return render(request, 'website/page.html', {'page': page})
