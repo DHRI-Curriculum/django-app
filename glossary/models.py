ef3986a53ef6050cc8b24c53dca5e8198cfda90a
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-08-04 16:59:10 -0400

    adding slug

--- glossary/models.py
+++ glossary/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Term(models.Model):
     term = models.TextField()
+    slug = models.CharField(max_length=200, blank=True)
     explication = models.TextField()
     readings = models.ManyToManyField('library.Reading')
     tutorials = models.ManyToManyField('library.Tutorial')
+
+    def save(self, *args, **kwargs):
+      name = self.name.replace('-',' ').replace('/',' ')
+      self.slug = slugify(name)
+      super(Workshop, self).save()
