3c500e81d7fb270e7d1ee10e4b5165959f2e8810
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-08-05 15:56:16 -0400

    testing auto search for glossary term

--- glossary/views.py
+++ glossary/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Term
+from lesson.models import Lesson
 
 def term(request, slug=None):
     obj = Term.objects.filter(slug=slug).last()
-    return render(request, 'glossary/term.html', {'term': obj})
+    lessons_with_term = Lesson.objects.filter(text__contains=f' {obj.term} ')
+    return render(request, 'glossary/term.html', {'term': obj, 'lessons_with_term': lessons_with_term})
 
 def index(request):
     terms = Term.objects.all()
