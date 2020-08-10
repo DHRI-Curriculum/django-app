2e0bc13536128d7d4b7a5df0e017ebbe1023a3b5
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-08-10 09:48:27 -0400

    last changes from last week

--- workshop/admin.py
+++ workshop/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Workshop
+from .models import Workshop, Frontmatter, Contributor, Praxis
 
 admin.site.register(Workshop)
+admin.site.register(Praxis)
+admin.site.register(Frontmatter)
+admin.site.register(Contributor)
