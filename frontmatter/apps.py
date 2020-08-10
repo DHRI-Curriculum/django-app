f438ac68e094ebccee4faf66a7c77db14f78dfcf
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-08-10 09:50:42 -0400

    final removing of folders

--- frontmatter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FrontmatterConfig(AppConfig):
-    name = 'frontmatter'
--- praxis/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PraxisConfig(AppConfig):
-    name = 'praxis'
