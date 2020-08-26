1fbf61d9f86e0d1edae1da5add056ce31e5bd29f
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-08-26 00:27:09 -0400

    lot of work on this app

--- backend/management/commands/loadglossary.py
+++ backend/management/commands/loadglossary.py
@@ -11,54 +11,23 @@ log = Logger(name='loadglossary')
 def create_terms(glossary_repo=GLOSSARY_REPO):
     loader = GlossaryLoader(glossary_repo)
 
-    for _, data in loader.sections.items():
-        readings, tutorials = list(), list()
+    for term in loader.all_terms:
 
-        if data.get('readings') != None and len(data.get('readings')):
-            readings = data.pop('readings')
-        elif data.get('readings') != None:
-            del data['readings']
-
-        if data.get('reading') != None and len(data.get('reading')):
-            readings = data.pop('reading')
-        elif data.get('reading') != None:
-            del data['reading']
-
-        if data.get('tutorials') != None and len(data.get('tutorials')):
-            tutorials = data.pop('tutorials')
-        elif data.get('tutorials') != None:
-            del data['tutorials']
-
-        if data.get('tutorial') != None and len(data.get('tutorial')):
-            tutorials = data.pop('tutorial')
-        elif data.get('tutorial') != None:
-            del data['tutorial']
-
-        if len(data) == 1:
-            header = [_ for _ in data.keys()][0]
-            content = [_ for _ in data.values()][0]
-        else:
-            log.error(f'Too many headers in the glossary term `{data}`. Please make sure the markdown file follows conventions.')
-
-        term, created = Term.objects.get_or_create(
-            term=header,
-            explication=content
+        t, created = Term.objects.get_or_create(
+            term=loader.terms[term].term,
+            explication=loader.terms[term].explication
         )
-        log.created(created, 'Term', term.term, term.id)
+        log.created(created, 'Term', t.term, t.id)
 
-        for annotation in readings:
-            title, url = process_links(annotation, 'reading')
-            if title == None: continue # TODO: Warning here?
-            obj, created = Reading.objects.get_or_create(annotation = annotation, title = title, url = url)
+        for d in loader.terms[term].readings:
+            obj, created = Reading.objects.get_or_create(annotation = d['annotation'], title = d['linked_text'], url = d['url'])
             log.created(created, 'Reading', obj.title, obj.id)
-            term.readings.add(obj)
+            t.readings.add(obj)
 
-        for annotation in tutorials:
-            label, url = process_links(annotation, 'tutorial')
-            if label == None: continue # TODO: Warning here?
-            obj, created = Tutorial.objects.get_or_create(annotation = annotation, label = label, url = url)
+        for d in loader.terms[term].tutorials:
+            obj, created = Tutorial.objects.get_or_create(annotation = d['annotation'], label = d['linked_text'], url = d['url'])
             log.created(created, 'Tutorial', obj.label, obj.id)
-            term.tutorials.add(obj)
+            t.tutorials.add(obj)
 
 
 class Command(BaseCommand):
