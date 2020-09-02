692c69fce3bb717a8e6df536dbfe08021d5189ee
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-09-01 22:11:22 -0400

    Renaming app and plugging it in

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     # path('glossary/', include('glossary.urls')),
     path('feedback/', include('feedback.urls')),
     path('install/', include('install.urls')),
+    path('insight/', include('insight.urls')),
     path('sign-up/', learner_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='learner/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='learner/logout.html'), name='logout'),
---
+++ insight/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def index(request):
+    pass
+
+def insight(request):
+    pass
