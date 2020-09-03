b3abe550a29fc1682afb09c22d104abbbe003998
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-09-03 10:32:42 -0400

    animation of left hand menu

--- website/static/website/js/base-left-menu.js
+++ website/static/website/js/base-left-menu.js
@@ -1,21 +1,32 @@
 const left_menubar = document.querySelector('#left-menu-menubar');
+const left_menu_wrapper = document.querySelector('#left-menu-wrapper');
 const left_main = document.querySelector('#left-menu-main');
 const toggle_menu_btn = document.querySelector('#toggle_menu');
 
 const hide_menubar = () => {
+    /*
     left_menubar.classList.add('d-none');
     left_menubar.classList.remove('d-block');
     left_main.classList.replace('col-md-9', 'col-md-12');
+    */
+   console.log('hide!');
+   left_menu_wrapper.style.left = '-500px';
+   left_main.classList.replace('col-md-9', 'col-md-12');
 };
 
 const show_menubar = () => {
+    /*
     left_menubar.classList.remove('d-none');
     left_menubar.classList.add('d-block');
     left_main.classList.replace('col-md-12', 'col-md-9');
+    */
+   console.log('show!');
+   left_menu_wrapper.style.left = '0px';
+   left_main.classList.replace('col-md-12', 'col-md-9');
 };
 
 const toggle_menu = (e) => {
-    if (left_menubar.classList.contains('d-none')) {
+    if (left_menu_wrapper.style.left == '-500px') {
         e.target.innerHTML = '&laquo; Hide left-hand menu';
         show_menubar()
     } else {
