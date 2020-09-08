06124641b3bc76ef592e9dc67b4726115f064243
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-09-08 11:38:32 -0400

    Adding sql connection

--- app/settings.py
+++ app/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'sql.cnf'), # '/etc/mysql/my.cnf',
+        },
     }
 }
 
