a8c52e3dc585bcc109a4f33c2c44f5b136d29209
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-09-11 19:42:54 -0400

    Last updates to solve #192 Switch to class-based views

--- website/urls.py
+++ website/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from . import views
+from views import Index, PageView
 
 app_name = 'website'
 
 urlpatterns = [
-  path('', views.Index.as_view(), name='index'),
-  path('<str:slug>/', views.PageView.as_view(), name='page'),
+  path('', Index.as_view(), name='index'),
+  path('<str:slug>/', PageView.as_view(), name='page'),
 ]
