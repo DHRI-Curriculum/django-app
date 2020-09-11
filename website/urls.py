5370a7b5f942ca89a37b2d06ccb5b255cbc86fe3
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-09-11 19:46:59 -0400

    fixing two errors related to #192

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from views import Index, PageView
+from .views import Index, PageView
 
 app_name = 'website'
 
--- workshop/urls.py
+++ workshop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from views import FrontmatterView, PraxisView, LessonView
+from .views import FrontmatterView, PraxisView, LessonView
 
 app_name = 'workshop'
 
