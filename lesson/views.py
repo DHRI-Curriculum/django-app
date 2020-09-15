7df18e9b4013fdaf21a6f072089b72fa9ac40c1d
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-09-15 16:14:21 -0400

    Fixes #198

---
+++ lesson/views.py
@@ -0,0 +1,32 @@
+import json
+
+from django.views.generic import View
+from django.http import JsonResponse
+
+from .models import Answer
+
+class CheckAnswer(View):
+
+    def post(self, request, *args, **kwargs):
+        print(request.headers)
+        question_id = request.headers.get('Question')
+        answers = request.headers.get('Answers')
+        try:
+            answers = json.loads(answers)
+        except:
+            return JsonResponse({'error': 'could not interpret JSON data'})
+
+        correct = dict()
+
+        for answer_id, response in answers.items():
+            a = Answer.objects.get(pk=answer_id)
+            print(a, answer_id, response, a.is_correct)
+            correct[answer_id] = response == a.is_correct
+
+        return JsonResponse({
+            'error': None,
+            'question_id': question_id,
+            'answers_provided': answers,
+            'correct_answers': correct,
+            'all_correct': all(x == True for x in correct.values())
+        })
