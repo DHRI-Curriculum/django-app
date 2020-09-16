214cd3afb3410d14f20f8ea2e49afc28163dba19
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-09-16 13:47:59 -0400

    Add message when on "wrong" installation page Fixes #203

--- install/views.py
+++ install/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Instruction
+from .models import Instruction, Software
+from django.urls import reverse
 
 
 def get_installation_menu_items():
@@ -34,4 +35,13 @@ class Detail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context = {**context, **get_installation_menu_items()}
+
+        other_os, context['other_os_slug'] = None, None
+        if 'macos' in context['instruction'].software.operating_system.lower(): other_os = 'Windows'
+        elif 'windows' in context['instruction'].software.operating_system.lower(): other_os = 'macOS'
+
+        if other_os:
+            current_software = context['instruction'].software.software
+            context['other_os_slug'] = Software.objects.get(software=current_software, operating_system=other_os).instructions.all().last().slug
+
         return context
