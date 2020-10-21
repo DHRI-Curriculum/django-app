cd6acd68f0720f960a3f7aac2094ee50b715eaac
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-10-21 18:10:53 -0400

    email unique

--- learner/forms.py
+++ learner/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class LearnerRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,3 +11,9 @@ class LearnerRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
+
+    def clean(self):
+       email = self.cleaned_data.get('email')
+       if User.objects.filter(email=email).exists():
+            raise ValidationError("Email address has to be unique (this address already exists).")
+       return self.cleaned_data
