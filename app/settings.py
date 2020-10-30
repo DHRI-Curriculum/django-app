3965924caee13bea1bf89c18ec99b0b2cff8bfe4
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-10-30 15:37:30 -0400

    adding correct URL

--- app/settings.py
+++ app/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILE_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = '/home/dhinstit/static.dhinstitutes.org/curriculum'
+STATIC_URL = 'http://static.dhinstitutes.org/curriculum/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
