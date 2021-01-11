fa09877dc17148e63f8181b1f52760ee84142ee4
Kalle Westerling <kwesterling1@gradcenter.cuny.edu>
2021-01-11 17:15:05 -0500

    testing solution here as well

--- feedback/models.py
+++ feedback/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from workshop.models import Workshop
-from lesson.models import Lesson
 
 class Issue(models.Model):
+    from workshop.models import Workshop
+    from lesson.models import Lesson
+
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='issues')
     workshop = models.ForeignKey(Workshop, on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='issues', null=True, blank=True)
