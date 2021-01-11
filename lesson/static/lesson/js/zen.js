c99230cd91f8a7598acbdc22c26aeb20e64d08cc
Kalle Westerling <kwesterling1@gradcenter.cuny.edu>
2021-01-11 14:57:16 -0500

    Work on #252

--- lesson/static/lesson/js/zen.js
+++ lesson/static/lesson/js/zen.js
@@ -1,5 +1,5 @@
-const zen_off = '<svg width="2em" height="2em" viewBox="0 0 16 16" class="pr-1 zen-bi bi bi-toggle-off" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11 4a4 4 0 0 1 0 8H8a4.992 4.992 0 0 0 2-4 4.992 4.992 0 0 0-2-4h3zm-6 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zM0 8a5 5 0 0 0 5 5h6a5 5 0 0 0 0-10H5a5 5 0 0 0-5 5z"/></svg> Zen mode'
-const zen_on = '<svg width="2em" height="2em" viewBox="0 0 16 16" class="pr-1 zen-bi bi bi-toggle-on" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"/></svg> Zen mode'
+const zen_off = '' // <svg width="2em" height="2em" viewBox="0 0 16 16" class="pr-1 zen-bi bi bi-toggle-off" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11 4a4 4 0 0 1 0 8H8a4.992 4.992 0 0 0 2-4 4.992 4.992 0 0 0-2-4h3zm-6 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zM0 8a5 5 0 0 0 5 5h6a5 5 0 0 0 0-10H5a5 5 0 0 0-5 5z"/></svg>'
+const zen_on = '' // <svg width="2em" height="2em" viewBox="0 0 16 16" class="pr-1 zen-bi bi bi-toggle-on" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"/></svg>'
 var nav_elements = document.getElementsByClassName('zen-add');
 let body = document.querySelector('body');
 
