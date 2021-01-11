9e16320b05db93d883e36d727eda455a81615025
Kalle Westerling <kwesterling1@gradcenter.cuny.edu>
2021-01-11 17:07:22 -0500

    something is more problematic

--- feedback/models.py
+++ feedback/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from workshop.models import Workshop
+from lesson.models import Lesson
 
 class Issue(models.Model):
-    # lesson = models.ForeignKey('lesson.Lesson', on_delete=models.CASCADE, null=True, blank=True, related_name='issues')
-    workshop = models.ForeignKey('workshop.Workshop', on_delete=models.CASCADE, null=True, blank=True)
+    # lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name='issues')
+    # workshop = models.ForeignKey(Workshop, on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='issues', null=True, blank=True)
     website = models.BooleanField(default=False)
     open = models.BooleanField(default=True)
