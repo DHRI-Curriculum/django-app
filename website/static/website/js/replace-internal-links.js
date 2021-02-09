16cab915bc291346d46227eded45b679076261c2
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2021-02-09 13:14:59 -0300

    Better code #297

--- website/static/website/js/replace-internal-links.js
+++ website/static/website/js/replace-internal-links.js
@@ -2,10 +2,10 @@
 document.querySelectorAll("[data-replace-internal-links=true]").forEach((elem) => {
     console.info('Encountered element and will recplace workshop links.');
     elem.querySelectorAll("a").forEach((link) => {
-        console.info('checking: ', link);
+        console.info('checking: ' + link);
         if (link.href.includes("github.com/DHRI-Curriculum/")) {
             finder = link.href.split("/")[link.href.split("/").length - 1];
-            console.log(finder);
+            console.log('Found and will replace link to ' + finder);
             if (!finder.includes(".md")) {
                 // it's a workshop if finder == any of the workshops
                 if (window.all_workshops_slugs[finder] != undefined && window.all_workshops_slugs[finder] != '') {
