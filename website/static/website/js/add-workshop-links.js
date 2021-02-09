4c4cfb7136b3c5e84ba9e1379a96c32d08790747
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2021-02-09 12:51:31 -0300

    Partial bug fix for #297

--- website/static/website/js/add-workshop-links.js
+++ website/static/website/js/add-workshop-links.js
@@ -1,7 +1,4 @@
 // all_workshops = [{% for workshop in all_workshops %}'{{workshop.name}}',{% endfor %}]
-all_workshops_slugs = {
-    {% for workshop in all_workshops %}'{{workshop.slug}}': '{% url 'workshop:frontmatter' workshop.slug %}',{% endfor %}
-}
 document.querySelectorAll("[data-replace-workshop-links=true]").forEach((elem) => {
     console.log(elem.dataset.replaceWorkshopLinks)
     elem.querySelectorAll("a").forEach((link) => {
@@ -9,8 +6,8 @@ document.querySelectorAll("[data-replace-workshop-links=true]").forEach((elem) =
             finder = link.href.split("/")[link.href.split("/").length - 1];
             if (!finder.includes(".md")) {
                 // it's a workshop if finder == any of the workshops
-                if (all_workshops_slugs[finder] != undefined && all_workshops_slugs[finder] != '') {
-                    link.href = all_workshops_slugs[finder];
+                if (window.all_workshops_slugs[finder] != undefined && window.all_workshops_slugs[finder] != '') {
+                    link.href = window.all_workshops_slugs[finder];
                 }
             }
         }
