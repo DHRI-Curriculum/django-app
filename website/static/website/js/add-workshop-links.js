dd9ccf09f6761e854215b6bffc7b2d86e19f5aeb
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2021-02-09 12:52:55 -0300

    Adding info output instead

--- website/static/website/js/add-workshop-links.js
+++ website/static/website/js/add-workshop-links.js
@@ -1,6 +1,6 @@
 // all_workshops = [{% for workshop in all_workshops %}'{{workshop.name}}',{% endfor %}]
 document.querySelectorAll("[data-replace-workshop-links=true]").forEach((elem) => {
-    console.log(elem.dataset.replaceWorkshopLinks)
+    console.info('Encountered element and will recplace workshop links.');
     elem.querySelectorAll("a").forEach((link) => {
         if (link.href.includes("github.com/DHRI-Curriculum/")) {
             finder = link.href.split("/")[link.href.split("/").length - 1];
