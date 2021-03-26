459a62182d49a03c4a4a43a40099217f89d0c359
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2021-03-26 12:02:48 -0300

    Adding jelastic specific runtime info

--- app/wsgi.py
+++ app/wsgi.py
@@ -9,6 +9,21 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+### Reclaim Cloud (Jelastic) specific runtime information ##############
+
+import sys
+virtenv = os.path.expanduser('~') + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv)) # This will only work on Python 3
+except IOError:
+    pass # TODO: We might want to do something here... exit application?
+
+sys.path.append(os.path.expanduser('~'))
+sys.path.append(os.path.expanduser('~') + '/ROOT/')
+
+#########################################################################
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
