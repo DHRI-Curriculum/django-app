0a914d858f56ce77c6dd060121c5932369bf30bf
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2021-03-26 12:14:28 -0300

    Clarifying jelastic code

--- app/wsgi.py
+++ app/wsgi.py
@@ -7,13 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
 ### Reclaim Cloud (Jelastic) specific runtime information ##############
 
+# Here's the activation of the environment. In `activate_this.py`, you should add lines that contain the secret keys below
+#    os.environ["SECRET_KEY"] = '<insert secret key>'
+#    os.environ["EMAIL_HOST_USER"] = '<insert email username>'
+#    os.environ["EMAIL_HOST_PASSWORD"] = '<insert email password>'
+#    os.environ["GITHUB_TOKEN"] = '<insert github token>'
+
+import os
 import sys
-virtenv = os.path.expanduser('~') + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+
+virtenv_dir = os.path.expanduser('~') + '/virtenv/'
+virtualenv = os.path.join(virtenv_dir, 'bin/activate_this.py')
+
 try:
     exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv)) # This will only work on Python 3
 except IOError:
@@ -24,8 +31,13 @@ sys.path.append(os.path.expanduser('~') + '/ROOT/')
 
 #########################################################################
 
+### Django standard runtime information #################################
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+
+#########################################################################
