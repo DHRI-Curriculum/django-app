96ef92180056cb7561dd8fb37b9f6993dbcb3d05
Stephen Zweibel <srzweibel@gmail.com>
2021-11-17 16:03:04 -0500

    Revert "Merge pull request #510 from DHRI-Curriculum:kallewesterling/issue506"
    This reverts commit 70d88dd2c52c35c41b3945313e00a2c8000fd788, reversing
    changes made to c0abfed14fdb640897cc2256fbd8a0df7b2248ba.

--- backend/urls.py
+++ backend/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
-
 # from django.views.generic import RedirectView
 
 from . import views
 
-app_name = "backend"
+app_name = 'backend'
 
 urlpatterns = [
-    path("", views.IndexRedirect.as_view(), name="index"),
-    path("debug/", views.DebugView.as_view()),
-    path("term/<str:slug>/", views.TermRedirectView.as_view()),
-    path("insight/<str:slug>/", views.InsightRedirectView.as_view()),
-    path("install/<str:slug>/", views.InstallRedirectView.as_view()),
-    path("workshop/<str:slug>/<str:praxis>", views.WorkshopRedirectView.as_view()),
+  path('', views.IndexRedirect.as_view(), name='index'),
+  path('debug/', views.DebugView.as_view()),
+  path('term/<str:slug>/', views.TermRedirectView.as_view()),
+  path('insight/<str:slug>/', views.InsightRedirectView.as_view()),
+  path('install/<str:slug>/', views.InstallRedirectView.as_view()),
+  path('workshop/<str:slug>/', views.WorkshopRedirectView.as_view()),
 ]
